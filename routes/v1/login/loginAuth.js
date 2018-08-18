de1b41455793bba499aa135c71e8533c5eea3873
孙明杰 <1964450091@qq.com>
2018-08-18 16:05:20 +0800

    %HEAD modify login api in deal leancloud response message %FINISHED%NEW = modify login api in deal leancloud response message

--- routes/v1/login/loginAuth.js
+++ routes/v1/login/loginAuth.js
@@ -4,34 +4,34 @@ var AV = require('leanengine');
 var cors = require('cors');
 var request=require('request');
 var corsOptions = {
-    origin: function(origin, callback){
-        console.log("LoginAuth# origin:"+origin);
-        var originIsWhitelisted = true;
-        callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
-    }
+	origin: function(origin, callback){
+		console.log("LoginAuth# origin:"+origin);
+		var originIsWhitelisted = true;
+		callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+	}
 };
 
 router.options('/', cors(corsOptions));
 router.post('/', cors(corsOptions), function(req, res, next) {
-    var authPlatform = req.body["authPlatform"];
-    var openid = req.body["openid"];
-    var access_token = req.body["access_token"];
-    var expires_in = req.body["expires_in"];
-    AV.User.loginWithAuthData({
-          openid: openid,
-          access_token: access_token,
-          expires_in: expires_in
-    }, authPlatform).then(function(user) {
-        var userinfo = user.toJSON();
-        userinfo.sessionToken = user._sessionToken;
-        console.log("Login# login success with user ", userinfo.username);
-        res.status(200);
-        res.send(userinfo);
-    }).catch(function(err) {
-        console.error("LoginAuth# error",err);
-        res.status(err.code);
-        res.send(err.message);
-    });
+	var authPlatform = req.body["authPlatform"];
+	var openid = req.body["openid"];
+	var access_token = req.body["access_token"];
+	var expires_in = req.body["expires_in"];
+	AV.User.loginWithAuthData({
+		  openid: openid,
+		  access_token: access_token,
+		  expires_in: expires_in
+	}, authPlatform).then(function(user) {
+		var userinfo = user.toJSON();
+		userinfo.sessionToken = user._sessionToken;
+		console.log("Login# login success with user ", userinfo.username);
+		res.status(200);
+		res.send(userinfo);
+	}).catch(function(err) {
+		console.error("LoginAuth# error",err);
+		res.status(err.code == 1? 401:error.code);
+		res.send(err.message.replace(/\[[^\)]*\]/g,""));
+	});
 });
 
 module.exports = router;
