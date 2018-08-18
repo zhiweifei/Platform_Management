bc48297eee9429045503c091181d562b5849a866
孙明杰 <1964450091@qq.com>
2018-08-18 16:06:12 +0800

    %HEAD modify user api in user name password and verify %FINISHED %NEW = modify user api in user name password and verify

--- routes/v1/user/user.js
+++ routes/v1/user/user.js
@@ -7,11 +7,11 @@ var AV = require('leancloud-storage');
 var router = require('express').Router();
 var cors = require('cors');
 var corsOptions = {
-    origin: function(origin, callback){
-        console.log("User#  origin:"+origin);
-        var originIsWhitelisted = true;
-        callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
-    }
+	origin: function(origin, callback){
+		console.log("User#  origin:"+origin);
+		var originIsWhitelisted = true;
+		callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+	}
 }
 
 var user_module = require('./lib/user-module');
@@ -20,76 +20,83 @@ var user_module = require('./lib/user-module');
 router.options('/', cors(corsOptions));
 // http request method get
 router.get('/', cors(corsOptions), function(req, res, next) {
-    var UM = new user_module(req);
-    AV.User.become(UM.sessionToken).catch(function () {
-        throw new AV.Error(401,'Invalid SessionToken');
-    }).then(function(){
-        UM.typeCheck();
-        return UM.getUserInfo()
-    }).then(function (td) {
-        var GroupResponse = [];
-        td.forEach(function (currentValue) {
-            GroupResponse.push(currentValue.toJSON());
-        });
-        res.status(200);
-        res.send(GroupResponse);
-    }).catch(function (error) {
-        res.status(error.code);
-        res.send(error.message);
-    })
+	var UM = new user_module(req);
+	AV.User.become(UM.sessionToken).catch(function () {
+		throw new AV.Error(401,'Invalid SessionToken');
+	}).then(function(){
+		UM.typeCheck();
+		return UM.getUserInfo()
+	}).then(function (td) {
+		var GroupResponse = [];
+		td.forEach(function (currentValue) {
+			GroupResponse.push(currentValue.toJSON());
+		});
+		res.status(200);
+		res.send(GroupResponse);
+	}).catch(function (error) {
+		res.status(error.code);
+		res.send(error.message);
+	})
 });
 
 // http request method post
 router.post('/', cors(corsOptions), function(req, res, next) {
-    var UM = new user_module(req);
-    UM.typeCheck();
-    UM.buildUser().then(function(user){
-        return UM.setUserACL(user)
-    }).then(function (newuser) {
-        return UM.relationGroup(newuser)
-    },function (error) {
-        throw error
-    }).then(function () {
-        res.status(201);
-        res.send("success, build up new User successfully");
-    }).catch(function (error) {
-        res.status(error.code);
-        res.send(error.message);
-    })
+	var UM = new user_module(req);
+	new Promise(function(resolve,reject){
+		UM.paramCheck();
+		UM.typeCheck();
+		resolve();
+	}).then(function(){
+		return UM.buildUser()
+	}).then(function(user){
+		return UM.setUserACL(user)
+	}).then(function (newuser) {
+		return UM.relationGroup(newuser)
+	},function (error) {
+		throw error
+	}).then(function () {
+		res.status(201);
+		res.send("success, build up new User successfully");
+	}).catch(function (error) {
+		res.status(error.code);
+		res.send(error.message);
+	})
 });
 
 // http request method put
 router.put('/', cors(corsOptions), function(req, res, next) {
-    var UM = new user_module(req);
-    AV.User.become(UM.sessionToken).catch(function () {
-        throw new AV.Error(401,'Invalid SessionToken');
-    }).then(function() {
-        return UM.updateUser();
-    }).then(function () {
-        res.status(201);
-        res.send("success, update user Info successfully");
-    }).catch(function (error) {
-        res.status(error.code);
-        res.send(error.message);
-    });
+	var UM = new user_module(req);
+	AV.User.become(UM.sessionToken).catch(function () {
+		throw new AV.Error(401,'Invalid SessionToken');
+	}).then(function() {
+		UM.paramCheck();
+		UM.typeCheck();
+		return UM.updateUser();
+	}).then(function () {
+		res.status(201);
+		res.send("success, update user Info successfully");
+	}).catch(function (error) {
+		res.status(error.code);
+		res.send(error.message);
+	});
 });
 
 // http request method delete
 router.delete('/', cors(corsOptions), function(req, res, next) {
-    var UM = new user_module(req);
-    AV.User.become(UM.sessionToken).catch(function () {
-        throw new AV.Error(401,'Invalid SessionToken');
-    }).then(function() {
-        UM.typeCheck();
-        return UM.deleteAllUser()
-    }).then(function () {
-        res.status(204);
-        // response get null
-        res.send("success, delete user successfully");
-    }).catch(function (error) {
-        res.status(error.code);
-        res.send(error.message);
-    });
+	var UM = new user_module(req);
+	AV.User.become(UM.sessionToken).catch(function () {
+		throw new AV.Error(401,'Invalid SessionToken');
+	}).then(function() {
+		UM.paramCheck();
+		UM.typeCheck();
+		return UM.deleteAllUser()
+	}).then(function () {
+		res.status(204);
+		res.send("success, delete user successfully");
+	}).catch(function (error) {
+		res.status(error.code);
+		res.send(error.message);
+	});
 });
 
 
--- routes/v1/user/verify.js
+++ routes/v1/user/verify.js
@@ -4,11 +4,11 @@ var AV = require('leancloud-storage');
 var router = require('express').Router();
 var cors = require('cors');
 var corsOptions = {
-    origin: function(origin, callback){
-        console.log("UserVerify#  origin:"+origin);
-        var originIsWhitelisted = true;
-        callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
-    }
+	origin: function(origin, callback){
+		console.log("UserVerify#  origin:"+origin);
+		var originIsWhitelisted = true;
+		callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+	}
 }
 
 var user_module = require('./lib/user-module');
@@ -17,21 +17,15 @@ var user_module = require('./lib/user-module');
 router.options('/', cors(corsOptions));
 // http request method post
 router.post('/', cors(corsOptions), function(req, res, next) {
-    var UM = new user_module(req);
-    AV.User.become(UM.sessionToken).then(function(user){
-        UM.user = user;
-    }).catch(function () {
-        throw new AV.Error(401,'Invalid SessionToken');
-    }).then(function() {
-        return UM.verifyEmailPhone();
-    }).then(function () {
-        res.status(201);
-        res.send("success, verify email or phone success");
-    }).catch(function (error) {
-        console.error('UserVerify #/user/verify put error',error);
-        res.status(error.code);
-        res.send(error.message);
-    });
+	var UM = new user_module(req);
+	UM.verifyEmailPhone().then(function () {
+		res.status(201);
+		res.send("success, verify email or phone success");
+	}).catch(function (error) {
+		console.error('UserVerify #/user/verify put error',error);
+		res.status(error.code == 1? 401:error.code);
+		res.send(error.message.replace(/\[[^\)]*\]/g,""));
+	});
 });
 
 
