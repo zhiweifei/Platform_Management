e50fa8d1f7f34f88a3f41f3a5010adfc12b461b5
孙明杰 <1964450091@qq.com>
2018-08-18 17:35:50 +0800

    %HEAD correct code style %FINISHED %BUG = Babel-92

--- routes/v1/login/login.js
+++ routes/v1/login/login.js
@@ -4,27 +4,27 @@ var AV = require('leanengine');
 var cors = require('cors');
 var request=require('request');
 var corsOptions = {
-	origin: function(origin, callback){
-		console.log("Login# origin:"+origin);
-		var originIsWhitelisted = true;
-		callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
-	}
+    origin: function(origin, callback){
+        console.log("Login# origin:"+origin);
+        var originIsWhitelisted = true;
+        callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+    }
 };
 router.options('/', cors(corsOptions));
 router.post('/', cors(corsOptions), function(req, res, next) {
-	var username = req.body['username'];
-	var password = req.body['password'];
-	AV.User.logIn(username,password).then(function(user){
-		var userinfo = user.toJSON();
-		userinfo.sessionToken = user._sessionToken;
-		console.log("Login# login success with user ", userinfo.username);
-		res.status(200);
-		res.send(userinfo);
-	}).catch(function(err){
-		console.error("Login# error",err);
-		res.status(err.code == 1? 401:error.code);
-		res.send(err.message.replace(/\[[^\)]*\]/g,""));
-	})
+    var username = req.body['username'];
+    var password = req.body['password'];
+    AV.User.logIn(username,password).then(function(user){
+        var userinfo = user.toJSON();
+        userinfo.sessionToken = user._sessionToken;
+        console.log("Login# login success with user ", userinfo.username);
+        res.status(200);
+        res.send(userinfo);
+    }).catch(function(err){
+        console.error("Login# error",err);
+        res.status(err.code == 1? 401:error.code);
+        res.send(err.message.replace(/\[[^\)]*\]/g,""));
+    })
 });
 
 module.exports = router;
