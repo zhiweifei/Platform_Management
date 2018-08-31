b9c9017560cc0bc07bf14af6a059647092c97a15
孙明杰 <1964450091@qq.com>
2018-08-31 18:19:23 +0800

    %HEAD add group-user and group-node api %FINISHED %NEW = Babel-92

---
+++ routes/v1/group/groupUser.js
@@ -0,0 +1,37 @@
+'use strict';
+var router = require('express').Router();
+var AV = require('leanengine');
+var cors = require('cors');
+var request=require('request');
+var corsOptions = {
+	origin: function(origin, callback){
+		console.log("GroupUser# origin:"+origin);
+		var originIsWhitelisted = true;
+		callback(originIsWhitelisted ? null : 'Bad Request', originIsWhitelisted);
+	}
+};
+var group_module = require('./lib/group-module');
+
+router.options('/', cors(corsOptions));
+
+router.get('/', cors(corsOptions), function(req, res, next) {
+    var GM = new group_module(req);
+    AV.User.become(GM.sessionToken).catch(function () {
+        throw new AV.Error(401,'Invalid SessionToken');
+    }).then(function(){
+        GM.typeCheck();
+        return GM.getGroupByname()
+    }).then(function (groups) {
+        return GM.getMidGroupUser(groups)
+    }).then(function(users){
+        res.status(200);
+        res.send(users);
+    }).catch(function (error) {
+        console.error('GroupUser# get error',error);
+        res.status(error.code);
+        res.send(error.message);
+    })
+
+});
+module.exports = router;
+
