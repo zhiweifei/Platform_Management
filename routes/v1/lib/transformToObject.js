640377ab79b85add05139c285b4c15a9a6560417
孙明杰 <1964450091@qq.com>
2018-09-04 17:31:12 +0800

    %HEAD modify transformToObject %FINISHED %MODIFY = Babel-92

--- routes/v1/lib/transformToObject.js
+++ routes/v1/lib/transformToObject.js
@@ -2,26 +2,10 @@
 var AV = require('leanengine');
 var async = require('async');
 var transformToObject = function (ArrParam,table,field,sessionToken) {
-    return new Promise(function (resolve,reject) {
-        async.map(ArrParam, function (value, callback) {
-            var Query = new AV.Query(table);
-            Query.equalTo(field, value);
-            Query.find({'sessionToken':sessionToken}).then(function (result) {
-                console.log('transformToObject #result object', result);
-                callback(null, result);
-            });
-        }, function (err, result) {
-            if (err != null) {
-                reject(err);
-            }
-            else {
-                var allObject = [];
-                result.forEach(function (current) {
-                    allObject = allObject.concat(current)
-                });
-                resolve(allObject);
-            }
-        });
-    })
+    var Query = new AV.Query(table);
+    Query.containedIn(field, ArrParam);
+    return Query.find({'sessionToken':sessionToken}).then(function (result) {
+        return result
+    });
 };
 module.exports = transformToObject;
