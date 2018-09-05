f7ebca93767f36b0eb8784cfe90022d13008f23c
ytf0105 <1421752582@qq.com>
2018-09-05 16:15:19 +0800

    %HEAD modify login console %FINISH %MODIFY modify console

--- tests/FT/v1/login.spec.ts
+++ tests/FT/v1/login.spec.ts
@@ -34,7 +34,6 @@ let postWrongPassword_test = {
 describe('POST /v1/login', () => {
 
 	let appLogin = new AppLogin(devurl, devurlPath, port);
-	console.log('appLogin',appLogin);
 	afterEach(function(done) {
 		this.timeout(7000)
 		console.log("Waiting 6s to avoid too many request error")
@@ -43,20 +42,20 @@ describe('POST /v1/login', () => {
 		})
 	})
 
-	it('should return SessionToken', (done) => {
+	it('testcase1# should return SessionToken', (done) => {
 		appLogin.login(postData,
 			(data: any, statusCode: number) => {
-				console.log('login# data,statusCode',data,statusCode);
+				console.log('POST /v1/login testcase1# data statusCode',data,statusCode);
 				data.should.have.property("sessionToken")
 				statusCode.should.equal(200)
 				done()
 			});
 	});
 
-	it('Give error username, should Could not find user.', (done) => {
+	it('testcase2# Give error username, should Could not find user.', (done) => {
 		appLogin.login(postErrorUsername,
 			(data: any, statusCode: number) => {
-				console.log('login# data,statusCode',data,statusCode);
+				console.log('POST /v1/login testcase2# data statusCode',data,statusCode);
 				//data.should.have.property('error');
 				data.should.equal('Could not find user. ')
 				statusCode.should.equal(211)
@@ -64,32 +63,31 @@ describe('POST /v1/login', () => {
 			});
 	});
 
-	it('Give wrone username, should Could not find user.', (done) => {
+	it('testcase3# Give wrone username, should Could not find user.', (done) => {
 		appLogin.login(postWrongUsername,
 			(data: any, statusCode: number) => {
-				console.log('login# data,statusCode',data,statusCode);
-				//data.should.have.property('error');
+				console.log('POST /v1/login testcase3# data statusCode',data,statusCode);
 				data.should.equal('Internal server error. No information available. ')
 				statusCode.should.equal(401)
 				done()
 			});
 	});
 
-	it('Give wrong password, should return The username and password mismatch.', (done) => {
+	it('testcase4# Give wrong password, should return The username and password mismatch.', (done) => {
 		appLogin.login(postWrongPassword_test,
 			(data: any, statusCode: number) => {
-				console.log('login# data,statusCode',data,statusCode);
+				console.log('POST /v1/login testcase4# data statusCode',data,statusCode);
 				data.should.equal('The username and password mismatch. ');
 				statusCode.should.equal(210)
 				done()
 			});
 	});
 
-	it('login fail too much should return 登录失败次数超过限制，请稍候再试，或者通过忘记密码重设密码。', (done) => {
+	it('testcase5# login fail too much should return 登录失败次数超过限制，请稍候再试，或者通过忘记密码重设密码。', (done) => {
 
 		function ErrorLogin() {
 			appLogin.login(postWrongPassword_test_data_1, function(data: any, statusCode: number){
-				console.log('login# data,statusCode',data,statusCode)
+				console.log('POST /v1/login testcase5# data statusCode',data,statusCode);
 				if(statusCode == 219){
 					done()
 				}else{
